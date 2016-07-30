de6069df2e65ee5d2aa0561a6a02735026ef1e11
Zoryn Aaron <zoryn4163@gmail.com>
2016-07-30 14:52:27 -0400

    update version string b/c I always forget

--- ZOmicronInfo/Program.cs
+++ ZOmicronInfo/Program.cs
@@ -50,7 +50,7 @@ namespace ZOmicronInfo
 
         public static string[] Args { get; private set; }
 
-        public const string Version = "1.3";
+        public const string Version = "1.4";
 
         [STAThread]
         internal static void Main(string[] args)
